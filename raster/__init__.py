da44c7315454c5b210346ca68cbe623791231a9e
cpelley <cpelley.pub@gmail.com>
2019-02-04 13:13:22 +0000

    Added basic iris files

---
+++ raster/__init__.py
@@ -0,0 +1,8 @@
+# (C) British Crown Copyright 2019, Met Office
+from __future__ import (absolute_import, division, print_function)
+from six.moves import (filter, input, map, range, zip)  # noqa
+
+from ._raster import export_geotiff
+
+
+__version__ = '0.1.dev0'
